f869db150e36fcd15ca134ff29dc8bb63798f641
Lindokuhle Momoza <lindokuhle.momoza@umuzi.org>
2019-06-06 16:08:10 +0200

    second commit

--- spec/PasswordSpec.js
+++ spec/PasswordSpec.js
@@ -1,75 +1,59 @@
-//Password is valid
-
-describe("password_is_valid", function(){
-    
-  it("password should be larger than 8 characters", function(){
-      expect(password_is_valid("hdfTG12g")).toBe(true);
+//Password is ok
+describe('password_is_valid', function(){
+  it('password should be larger than 8 characters', function(){
+      let output = password_is_valid('lin237GHdh');
+      expect(output).toBe(true);
   })
-    
- 
- it("password should not be null", function(){
-      expect(password_is_valid("hsda23KJss")).toBe(true);
+  it('password should not be null', function(){
+      let output = password_is_valid('sa4231Dgh');
+      expect(output).toBe(true);
   })
-    
-    
- it("password should have at least one uppercase letter",function(){
-      expect(password_is_valid("ghFh367231")).toBe(true);
+  it('password should have at least one uppercase letter', function(){
+      let output = password_is_valid('liNDyM234');
+      expect(output).toBe(true);
   })
-    
- it("password should have at least one lowercase letter", function(){
-      expect(password_is_valid("Hj34245GD")).toBe(true);
+  it('password should have at least one lowercase letter', function(){
+      let output= password_is_valid('shaHeedaH23');
+      expect(output).toBe(true);
   })
-    
- it("password should have at least one number", function(){
-      expect(password_is_valid("hj5jaDEEjd")).toBe(true);
+  it('password should have at least one number', function(){
+      let output= password_is_valid('lin3454dFf');
+      expect(output).toBe(true);
   })
-    
 });
- 
-
 
 //Password is ok
-
-
-describe("password_is_ok", function(){
-
-  it("password should be larger than 8 characters", function(){
-      expect(password_is_ok("hdfTG12g4")).toBe(true);
+describe('password_is_ok', function(){
+    
+  it('should check if password is greater than eight characters and has at least one uppercase ', function(){
+      let output = password_is_ok('H4ffLOgOOg');
+      expect(output).toBe('Password is valid');
   })
-    
- 
- it("password should not be null", function(){
-      expect(password_is_ok("hsda23KJss")).toBe(true);
+  it('should check if password is greater than eight characters and has at least one lowercase', function(){
+      let output= password_is_ok('jhs78whjs');
+      expect(output).toBe('Password is valid');
   })
-    
-    
- it("password should have at least one uppercase letter",function(){
-      expect(password_is_ok("ghGFh367231")).toBe(true);
+  it('should check if password is greater than eight characters and has atleast one number', function(){
+      let output = password_is_ok('dfhgw67bs7jK');
+      expect(output).toBe('Password is valid');
   })
-    
- it("password should have at least one lowercase letter", function(){
-      expect(password_is_ok("hjsa34245GD")).toBe(true);
+  it('should check if password has more than eight characters', function(){
+      let output= password_is_ok('Hasy367ghas');
+      expect(output).toBe('Password is valid');
   })
-    
- it("password should have at least one number", function(){
-      expect(password_is_ok("hj16jaDEEjd")).toBe(true);
-  })  
-
-
+  it('should check if password is greater than eight characters and has at least one upper and lower case letter and a number', function(){
+      let output = password_is_ok('Hsahrh43');
+      expect(output).toBe('Password is valid');
+  })
 });
 
-//never okay password
-describe("not_okay", function(){
+//never okay
 
-   it ("password should be larger than 8 characters", function(){
-        expect(not_okay("hj16jaDEEjd")).toBe(false);
+describe("never_ok(password)", function(){
 
+   it ("should have more than 8 characters and not be null", function(){
+       let output = never_ok("12dfga3");
+       expect(output).toBe("Password is never OK");
    })
-  
-    it ("password should not be null", function(){
-        expect(not_okay("hj16jaDEEjd")).toBe(false);
-
-   })
-  
 
 });
--- src/Password.js
+++ src/Password.js
@@ -1,31 +1,47 @@
-//password is valid
+//Password is valid
 function password_is_valid(password){
-   var validation = /^(?=.\d)(?=.[a-z])(?=.*[A-Z]).{9,}$/;
-   var output = validation.test(password);
+   var check = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{9,}$/
+   var result = check.test(password);
+  try {
+    if(result == false) throw('Password must contain atleast one uppercase letter, lowercase letter and a number');
+  } catch (error) {
+    return 'Error ' + error;
+  }
+   
+    return result;   
+ }
 
 
-    
-function password_is_ok(password){
-           
-           var upper = /[A-Z]/;
-          
-           var lower = /[a-z]/;
-           
-           var number = /[a-z]/;
-           
-           if(upper || lower || number){
-               return true;
-           } else if(two || one || four){
-               return true;
-           
-function not_okay(password){
-   if(password.length < 9 || password.length == 0){
-       return "Password is never OK";
-   }
-  
+//function is ok
+function password_is_ok(password) {
+ 
+ var pass1 = /^[A-Za-z0-9]{9,}$/;
+ var pass2 = /^(?=.*[A-Z])[A-Z]+/;
+ var pass3 = /^(?=.*[a-z])[a-z]+/;
+ var pass4 = /^(?=.*[0-9])[0-9]+/;
+ var pass5 = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{9,}$/;
+
+ if (pass1.test(password) === true && pass2.test(password) === true) {
+   return 'Password is valid';
+ } else
+ if (pass1.test(password) === true && pass3.test(password) === true) {
+   return 'Password is valid';
+ } else
+ if (pass1.test(password) === true && pass4.test(password) === true) {
+   return 'Password is valid';
+ } else{
+ if (pass5.test(password) === true)
+   return 'Password is valid';
+ }
 }
 
+//Password never ok
 
+function never_ok(password){
+     if(password.length < 9 || password.length == 0){
+       return "Password is never OK";
+   }
+} 
 
 
 
